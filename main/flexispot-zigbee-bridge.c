8ecb1fc3dd90f1758a0c071566255d2b177452b5
MaxBas <m.bastian2004@gmail.com>
2025-11-29 23:16:25 +0100

    First Zigbee setup

--- main/flexispot-zigbee-bridge.c
+++ main/flexispot-zigbee-bridge.c
@@ -1,7 +1,25 @@
+#include "flexispot-zigbee-bridge.h"
+
 #include "esp_err.h"
 #include "nvs_flash.h"
-#include "esp_zigbee_core.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "zcl/esp_zigbee_zcl_analog_value.h"
+
+void esp_zb_task(void *pvParameters)
+{
+    esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZED_CONFIG();
+    esp_zb_init(&zb_nwk_cfg);
+
+}
 
 void app_main(void)
 {
+    esp_zb_platform_config_t config = {
+        .radio_config = ESP_ZB_DEFAULT_RADIO_CONFIG(),
+        .host_config = ESP_ZB_DEFAULT_HOST_CONFIG()
+    };
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_zb_platform_config(&config));
+    xTaskCreate(esp_zb_task, "Zigbee_main", 4096, NULL, 5, nullptr);
 }
